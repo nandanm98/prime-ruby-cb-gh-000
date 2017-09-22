977e9abbfaa7bc5d2d66b29cbe332b3627c28462
Nandan M <nandanm98@gmail.com>
2017-09-22 16:05:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  i=2
+  count=0
+  while i < num
+    if num % i == 0
+      count+=1
+    end
+    i+=1
+  end
+  if count == 0
+    true
+  else
+    false
+  end
+end
